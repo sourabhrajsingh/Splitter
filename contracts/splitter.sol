99f6157daf9855363633aa1028099b22af6c83a8
sourabhrajsingh <sourabh.works@gmail.com>
2018-09-30 20:17:33 +0530

    rework for general purpose splitter

--- contracts/splitter.sol
+++ contracts/splitter.sol
@@ -1,51 +1,32 @@
 pragma solidity ^0.4.21;
 
 contract B9Splitter {
-    address public alice;
-    address public bob;
-    address public carol;
+    address public owner;
     mapping(address => uint) public balances;
-    
-    event logSplitFunds(address splitter, uint aliceHas, uint bobHas, uint carolHas);
+
+    event logSplitFunds(address splitter, uint splitterHas, uint receiver_1_Has, uint receiver_2_Has);
     event logWithdrawnFunds(address withdrawer, uint amountWithdrawn);
 
-    
-    constructor(address _address1, address _address2) public {
-        alice = msg.sender;
-        bob = _address1;
-        carol = _address2;
-    }
-    
-    modifier onlyAlice {
-         require( msg.sender == alice);
-         _;
-     }
-     
-    function split () public payable onlyAlice returns(bool success) {
-        require(msg.value > 0);
-        if (msg.value % 2 == 0) {
-            balances[bob] += (msg.value)/2;
-            balances[carol] += (msg.value)/2;
+
+    constructor() public {
+         owner = msg.sender;
+       }
+
+    function split (address _address1, address _address2) public payable returns(bool success) {
+        require(msg.value > 0 );
+        if (msg.value % 2 > 0)  balances[msg.sender] += 1;
+            balances[_address1] += (msg.value)/2;
+            balances[_address2] += (msg.value)/2;
             return true;
-         }
-        if (msg.value % 2 == 1) {
-            balances[bob] += (msg.value - 1)/2;
-            balances[carol] += (msg.value - 1)/2;
-            balances[alice] += 1;
-            return true;
-        }
-        emit logSplitFunds(msg.sender, balances[alice], balances[bob], balances[carol]);
+        emit logSplitFunds(msg.sender, balances[msg.sender], balances[_address1], balances[_address2]);
     }
-    
-    function withdrawFunds () public returns(bool success) {
-        uint amount = balances[msg.sender];
-        if(amount > 0) {
-            balances[msg.sender] = 0;
+
+    function withdrawFunds (uint amount) public returns(bool success) {
+        require(amount <= balances[msg.sender]);
+            balances[msg.sender] -= amount;
             msg.sender.transfer(amount);
             return true;
-        }
         emit logWithdrawnFunds(msg.sender, amount);
-        
+
     }
 }
-
