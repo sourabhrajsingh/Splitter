0cd72245b819c7cc2c9e59cdde09806c37d4f058
sourabhrajsingh <sourabh.works@gmail.com>
2018-10-01 13:16:10 +0530

    improved the event logging

--- contracts/splitter.sol
+++ contracts/splitter.sol
@@ -4,7 +4,7 @@ contract B9Splitter {
     address public owner;
     mapping(address => uint) public balances;
 
-    event logSplitFunds(address splitter, uint splitterHas, uint receiver_1_Has, uint receiver_2_Has);
+    event logSplitFunds(address splitter, address receiver_1, address receiver_2, uint amountSplit);
     event logWithdrawnFunds(address withdrawer, uint amountWithdrawn);
 
 
@@ -14,19 +14,21 @@ contract B9Splitter {
 
     function split (address _address1, address _address2) public payable returns(bool success) {
         require(msg.value > 0 );
-        if (msg.value % 2 > 0)  balances[msg.sender] += 1;
-            balances[_address1] += (msg.value)/2;
-            balances[_address2] += (msg.value)/2;
-            return true;
-        emit logSplitFunds(msg.sender, balances[msg.sender], balances[_address1], balances[_address2]);
+        if (msg.value % 2 > 0) {
+          balances[msg.sender] += 1;
+        }
+          balances[_address1] += (msg.value)/2;
+          balances[_address2] += (msg.value)/2;
+          emit logSplitFunds(msg.sender, _address1, _address2, msg.value);
+          return true;
+
     }
 
     function withdrawFunds (uint amount) public returns(bool success) {
         require(amount <= balances[msg.sender]);
-            balances[msg.sender] -= amount;
-            msg.sender.transfer(amount);
-            return true;
+        balances[msg.sender] -= amount;
         emit logWithdrawnFunds(msg.sender, amount);
-
+        msg.sender.transfer(amount);
+        return true;
     }
 }
